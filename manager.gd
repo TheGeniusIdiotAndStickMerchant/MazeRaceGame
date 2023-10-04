9069f7eb5aa9b316f99a8d14b144e6458a147399
TheGeniusIdiotAndStickMerchant <guusvanaaken@leerling.parmantscholen.nl>
2023-10-04 12:29:16 +0200

    Created a pause menu

--- manager.gd
+++ manager.gd
@@ -3,6 +3,7 @@ extends Node
 @export var maze_scene: PackedScene
 
 var maze: GameScene = null
+var last_init: Callable
 
 func _ready():
 	randomize()
@@ -21,25 +22,52 @@ func standard_game_stop():
 
 
 func _on_single_button_pressed():
+	last_init = _on_single_button_pressed
 	standard_game_init()
 	maze.random_marker_no_back()
 	standard_game_stop()
 
-
 func _on_single_button_back_pressed():
+	last_init = _on_single_button_back_pressed
 	standard_game_init()
 	maze.random_marker_back()
 	standard_game_stop()
 
-
 func _on_all_button_pressed():
+	last_init = _on_all_button_pressed
 	standard_game_init()
 	maze.all_markers_rand_order_auto_back()
 	standard_game_stop()
 
-
-
 func _on_all_button_back_pressed():
+	last_init = _on_all_button_back_pressed
 	standard_game_init()
 	maze.all_markers_rand_order_back()
 	standard_game_stop()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel") && maze != null:
+		$PauseMenu.show()
+		get_tree().paused = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func end_pause():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	get_tree().paused = false
+	$PauseMenu.hide()
+
+func _on_restart_button_pressed():
+	maze.queue_free()
+	last_init.call()
+	
+	end_pause()
+
+func _on_menu_button_pressed():
+	maze.queue_free()
+	$MainMenu.show()
+	$PauseMenu.hide()
+	get_tree().paused = false
+
+func _on_resume_button_pressed():
+	end_pause()
